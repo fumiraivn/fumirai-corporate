e5fb429471c4dbd95d735b7e8d5e5040babbe7e7
thuyhuynh-fumirai <thuy.huynh.van@fumirai.ltd>
2025-10-10 09:24:07 +0700

    Add Dropdown component and its associated types
    - Export Dropdown component from base index
    - Include DropdownOption and DropdownProps types for better type safety

--- src/components/base/index.tsx
+++ src/components/base/index.tsx
@@ -3,3 +3,5 @@ export { default as Button, ButtonVariant as ButtonVariant } from './Button';
 export type { ButtonProps } from './Button';
 export { default as Container } from './Container';
 export type { ContainerProps } from './Container';
+export { default as Dropdown } from './Dropdown';
+export type { DropdownOption, DropdownProps } from './Dropdown';
